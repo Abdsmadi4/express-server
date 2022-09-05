da5b3e0327da6253137b447bb9f0b048802aebdb
Abdsmadi4 <abd.smadi4@gmail.com>
2022-09-05 18:16:32 +0300

    test

---
+++ server.js
@@ -0,0 +1,28 @@
+"use strict";
+const express = require("express");
+
+const app = express();
+app.use(express.json());
+
+
+
+
+app.get('/', (req, res) => {
+  res.status(200).send("Hello World");
+});
+app.post("/person", (req, res) => {
+  const age = req.query.age;
+  const newAge = parseInt(age) + 5;
+  res.status(200).send(`new age = ${newAge}`);
+});
+
+
+function start(port) {
+  app.listen(port, () => console.log(`Listening on port ${port}`));
+
+}
+
+module.exports = {
+  app: app,
+  start: start,
+};
